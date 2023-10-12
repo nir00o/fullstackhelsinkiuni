d2c721d675efef4d1a92eb2fb7112f2972b97617
niroue <yowawifu3@gmail.com>
2023-10-12 10:24:39 +0200

    xxx

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,4 +1,31 @@
+const Header = (props) => {
+  return (
+    <div>
+    <h1>{props.course}</h1>
+    </div>
+  )
+}
+const Content = (props) => {
+  return (
+    <div>
+    <p>{props.part1} {props.exercises1}</p>
+    <p>{props.part2} {props.exercises2}</p>
+    <p> {props.part3} {props.exercises3}</p>
+    
+    </div>
+  )
+}
+const Total = (props) => {
+  
+  return (
+    <div>
+    <p>Number of exercises {props.allex}</p>
+    </div>
+  )
+}
+
 const App = () => {
+ 
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
   const exercises1 = 10
@@ -6,21 +33,19 @@ const App = () => {
   const exercises2 = 7
   const part3 = 'State of a component'
   const exercises3 = 14
+ const allex = 0
+  
+
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
-    </div>
+      <Header course = {course} />
+      <Content part1={part1} exercises1={exercises1}
+       part2={part2} exercises2={exercises2}
+       part3={part3} exercises3={exercises3}
+      />
+      <Total allex={exercises1 + exercises2 + exercises3} />
+   </div>
   )
 }
 
